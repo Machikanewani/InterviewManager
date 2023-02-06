23380c06369bb47c717199974c61746d8e05c1f9
Xu Shihao <xushihao@localhost>
2023-02-06 23:58:40 +0900

    add spring initializer demo

---
+++ InterviewManager/src/test/java/com/example/InterviewManager/InterviewManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InterviewManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
